824ce98f8beb8f91bc77def3a20bbb541db5ca52
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-13 22:44:33 -0800

    Updated the homepage and post page.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,2 +1,15 @@
 class BlogController < ApplicationController
+
+  def index
+    @posts = Blog.all.order("updated_at DESC")
+  end
+
+  def post
+    @post = Blog.find(params[:id])
+    @author = Author.find(@post.author)
+    @post.view_count += 1
+    @post.save
+    @comments = Comment.all
+  end
+
 end
