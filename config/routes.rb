f09461560ed68c8b8ac77c05b0510cb02ca4c317
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-13 18:19:20 -0800

    Added routes, and associated controllers and initial pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root "blog#index"
+  get "/post/:id" => "blog#post"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
